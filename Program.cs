fcddea6e0446eb4388d714fa8afc74d05efd78ba
Andre de Castro Rondi <slippandre@gmail.com>
2021-11-30 00:24:59 +0000

    Aula01

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            string nome = "André";
+            string sobrenome = "Rondi";
+            string nomeCompleto = nome + " " + sobrenome;
+            Console.WriteLine(nomeCompleto);
+
+            var valor = 42.50;
+            var taxa = valor * 0.10;
+            var total = valor + taxa;
+
+            Console.WriteLine($"O valor total é R$ {total:C}.");
         }
     }
 }
